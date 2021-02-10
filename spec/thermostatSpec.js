6a6737f693e202cc827008293448e8b2844409e2
Joshua Sinyor <josh.sinyor@gmail.com>
2021-02-10 17:16:49 +0000

    Update thermostat.js and thermostatSpec.js. Reflects addition of reset function.

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -5,43 +5,48 @@ describe('Thermostat', function() {
     thermostat = new Thermostat;
   });
 
-  it('starts at 20˚c', function(){
+  it('starts at 20˚C', function(){
     expect(thermostat.temperature).toEqual(20);
   });
 
-  it('rises 1˚c whenever up is called', function(){
+  it('rises 1˚C whenever up is called', function(){
     thermostat._up();
     expect(thermostat.temperature).toEqual(21);
   });
 
-  it('drops 1˚c whenever down is called', function(){
+  it('drops 1˚C whenever down is called', function(){
     thermostat._down();
     expect(thermostat.temperature).toEqual(19);
   });
 
-  it('raises an error when taking temperature below 10˚c', function(){
+  it('raises an error when taking temperature below 10˚C', function(){
     for (i = 0; i < 10; i++) {thermostat._down()};
     expect(function() {
-      thermostat._down()}).toThrow(new Error ('minimum temperature is 10˚c'));
+      thermostat._down()}).toThrow(new Error ('minimum temperature is 10˚C'));
   });
 
-  it('raises an error when taking temperature above 25˚c in power saving mode', function(){
+  it('raises an error when taking temperature above 25˚C in power saving mode', function(){
     for (i = 0; i < 5; i++) {thermostat._up()};
     expect(function() {
-      thermostat._up()}).toThrow(new Error ('maximum temperature in power saving mode is 25˚c'));
+      thermostat._up()}).toThrow(new Error ('maximum temperature in power saving mode is 25˚C'));
   });
 
-  it('raises an error when taking temperature above 32˚c', function(){
+  it('raises an error when taking temperature above 32˚C', function(){
     thermostat._changePowerSavingMode();
     for (i = 0; i < 12; i++) {thermostat._up()};
     expect(function() {
-      thermostat._up()}).toThrow(new Error ('maximum temperature is 32˚c'));
+      thermostat._up()}).toThrow(new Error ('maximum temperature is 32˚C'));
   });
 
   it('switches power saving mode', function(){
     expect(thermostat.powerSaving).toEqual(true);
     thermostat._changePowerSavingMode()
     expect(thermostat.powerSaving).toEqual(false);
+  });
+
+  it('resets the temperature to 20˚C when reset is called', function() {
+    thermostat._reset();
+    expect(thermostat.temperature).toEqual(20);
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -7,15 +7,15 @@ class Thermostat{
 
   _up() {
     if (this.powerSaving === true && this.temperature === 25)
-      {throw new Error ('maximum temperature in power saving mode is 25˚c')}
+      {throw new Error ('maximum temperature in power saving mode is 25˚C')}
     else if (this.powerSaving === false && this.temperature === 32)
-      {throw new Error ('maximum temperature is 32˚c')};
+      {throw new Error ('maximum temperature is 32˚C')};
     this.temperature += 1;
   };
 
   _down() {
     if (this.temperature === 10)
-      {throw new Error ('minimum temperature is 10˚c')}
+      {throw new Error ('minimum temperature is 10˚C')}
     else {this.temperature -= 1;};
   };
 
@@ -24,5 +24,10 @@ class Thermostat{
     if (this.powerSaving === true && this.temperature > 25 )
      {this.temperature = 25};
   };
+
+  _reset() {
+    this.temperature = 20;
+  };
+
 
 }
