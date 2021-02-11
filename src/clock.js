f4c212a9b35693e7b7062c585bde3fe3c9e70867
Joshua Sinyor <>
2021-02-11 19:15:10 +0000

    Update index.html, clock.js, jquery.js and stylesheet.css. Reflects refactoring and HTML/CSS.

--- src/clock.js
+++ src/clock.js
@@ -1,6 +1,7 @@
-const myVar = setInterval(myTimer, 500);
+const refreshVariable = setInterval(myTimer, 500);
 
 function myTimer() {
  const t = new Date();
  document.getElementById("clock-display").innerHTML = t.toLocaleTimeString();
+ document.getElementById("date-display").innerHTML = t.toLocaleDateString();
 };
