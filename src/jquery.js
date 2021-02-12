76c1d0b387205fe1ef8bd4cebb2d242532f6a6f4
RTurney <r.turney93@gmail.com>
2021-02-12 16:44:16 +0000

    All buttons functional. Refactor jquery functions.

--- src/jquery.js
+++ src/jquery.js
@@ -1,13 +1,42 @@
 $( document ).ready(function() {
   thermostat = new Thermostat;
 
+  const _updateTempDisplay = () => { $( "#temperature-display" ).html(thermostat.temperature) };
+  _updateTempDisplay();
+
+  const _updatePowerModeDisplay = () => {
+    if()
+  };
+
     $( "a" ).click(function( event ) {
 
         alert( "Thanks for visiting!" );
     });
 
     $( "#temperature-up" ).click( function() {
-      thermostat._up()
-      $( "#temperature-display" ).html(thermostat.temperature)
+      thermostat._up();
+      _updateTempDisplay();
+    });
+
+    $( "#temperature-down" ).click( function() {
+      thermostat._down();
+      _updateTempDisplay();
+    });
+
+    $( "#temperature-reset" ).click( function() {
+      thermostat._reset();
+      _updateTempDisplay();
+    });
+
+    $( "#powersavingmode" ).click( function() {
+      if(thermostat.powerSavingMode) {
+        thermostat._changePowerSavingMode();
+        $( "#powersavingmode-text" ).text("Power Save OFF");
+        _updateTempDisplay();
+      } else {
+        thermostat._changePowerSavingMode();
+        $( "#powersavingmode-text" ).text("Power Save");
+        _updateTempDisplay();
+      };
     });
 });
