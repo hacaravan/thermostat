1e9d4e185c125a25cbf64861486edd5a9af762eb
RTurney <r.turney93@gmail.com>
2021-02-12 15:47:16 +0000

    minor changes to jquery. Add alert and hide button functions

---
+++ src/jquery.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+
+    $( "a" ).click(function( event ) {
+
+        alert( "Thanks for visiting!" );
+    });
+
+    // $( "#temperature-up" ).click( function() {
+    //
+    //   $(this).hide();
+    // });
+});
